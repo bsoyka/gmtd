d522f3fbea6a81317ea1a8e0124273865eac4ef8
Ben Soyka <bensoyka@icloud.com>
2022-01-07 06:11:08 -0700

    :memo: Add more information to docstrings

--- gmtd/__init__.py
+++ gmtd/__init__.py
@@ -7,13 +7,34 @@ __version__ = "1.1.0b2"
 
 
 def _get_package_metadata(package: str) -> Dict[str, str]:
-    """Get a package's metadata in a parsable format"""
+    """Gets a package's metadata in a parsable format.
+
+    Args:
+        package: The name of the package to get metadata for.
+
+    Returns:
+        A dictionary of the package's metadata.
+
+    Raises:
+        PackageNotFoundError: If the package is not installed.
+    """
 
     return metadata(package).json
 
 
 def get_documentation(package: str) -> List[str]:
-    """Get the documentation URL for a package"""
+    """Gets the documentation URL for a package.
+
+    Args:
+        package: The name of the package to get documentation for.
+
+    Returns:
+        A list of possible documentation URLs for the package, ordered
+        from most to least likely to be official documentation.
+
+    Raises:
+        PackageNotFoundError: If the package is not installed.
+    """
 
     package_metadata = _get_package_metadata(package)
 
